b80717126f5863bfde742fd8284d49580c24c0c3
Camila <reinkecamila@gmail.com>
2026-05-04 21:58:11 -0300

    add SecurityConfiguration

---
+++ src/main/java/com/cr/spring_security/securities/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.cr.spring_security.securities;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}
