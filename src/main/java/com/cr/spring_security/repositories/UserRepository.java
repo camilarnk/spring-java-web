6552042730f12f475b363c7c7c0286a7a61c9245
Camila <reinkecamila@gmail.com>
2026-05-04 21:57:33 -0300

    add repository e service para User

---
+++ src/main/java/com/cr/spring_security/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cr.spring_security.repositories;
+
+import com.cr.spring_security.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByLogin(String role);
+
+}
