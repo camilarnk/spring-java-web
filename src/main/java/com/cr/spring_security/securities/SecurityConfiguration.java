0fddddcc79ee17ce0813965b827265423500289f
Camila <reinkecamila@gmail.com>
2026-05-04 22:10:26 -0300

    add anotação @Bean em SecurityConfiguration

--- src/main/java/com/cr/spring_security/securities/SecurityConfiguration.java
+++ src/main/java/com/cr/spring_security/securities/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
