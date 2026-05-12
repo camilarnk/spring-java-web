9c09a8d4b3d7d7a889bbd57eddfb75fa3eb29659
Camila <reinkecamila@gmail.com>
2026-05-12 19:30:07 -0300

    add bean import

--- src/main/java/com/cr/spring_security/securities/SecurityConfiguration.java
+++ src/main/java/com/cr/spring_security/securities/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.cr.spring_security.securities;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
