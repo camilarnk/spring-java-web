727047bf81a5ab090e3f15a32ac536bf50b6abc7
Camila <reinkecamila@gmail.com>
2026-05-12 20:27:38 -0300

    add JobModel + relaciona com PessoaModel

--- src/main/java/com/cr/spring_security/models/Job.java
+++ src/main/java/com/cr/spring_security/models/Job.java
@@ -1,26 +1,27 @@
 package com.cr.spring_security.models;
 
-import jakarta.persistence.*;
-
-import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Table(name = "tb_pessoas")
-public class Pessoa {
+public class Job {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String endereco;
 
-    @OneToMany
-    private List<Job> jobs = new ArrayList<>();
-
-    public Pessoa() {}
+    public Job() {}
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNome() {
