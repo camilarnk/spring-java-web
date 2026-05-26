60e9a199d558064dc6232b6436c4c010408d7d1a
Camila <reinkecamila@gmail.com>
2026-05-25 21:51:16 -0300

    feat: authorizeHttpRequests

--- src/main/java/com/cr/spring_security/securities/SecurityConfiguration.java
+++ src/main/java/com/cr/spring_security/securities/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.cr.spring_security.securities;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +17,8 @@ public class SecurityConfiguration {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(HttpMethod.GET, "/pessoas").permitAll())
                 .build();
     }
 
-}
