ecee0fc0fa0ed2ebca937ae65566a115a6b18648
Camila <reinkecamila@gmail.com>
2026-06-01 22:10:35 -0300

    docs: adiciona comentarios para anotações em controller

--- src/main/java/com/cr/spring_security/controllers/PessoaController.java
+++ src/main/java/com/cr/spring_security/controllers/PessoaController.java
@@ -11,14 +11,15 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/pessoas")
+@RestController // classifica a classe como controlador rest
+@RequestMapping("/pessoas") // define a rota endpoint
 public class PessoaController {
 
-    @Autowired
+    @Autowired // injeta as dependencias de PessoaService
     private PessoaService service;
 
-    @PostMapping
+    @PostMapping // define o metodo como post (cria um objeto)
+    // @RequestBody define que o objeto Pessoa sera passado pelo corpo da requisição
     public ResponseEntity<Pessoa> create(@RequestBody Pessoa pessoa) {
         Pessoa request = service.create(pessoa);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -27,13 +28,14 @@ public class PessoaController {
         return ResponseEntity.created(uri).body(request);
     }
 
-    @GetMapping
+    @GetMapping // define o metodo como get (busca todos objetos)
     public ResponseEntity<List<Pessoa>> findAll() {
         List<Pessoa> request = service.findAll();
         return ResponseEntity.ok().body(request);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") // define metodo como get por id
+    // @PathVariable esta indicando que o id sera passado pela url
     public ResponseEntity<Optional<Pessoa>> findById(@PathVariable Long id) {
         Optional<Pessoa> request = service.findById(id);
         if(request.isPresent()) {
@@ -42,13 +44,13 @@ public class PessoaController {
         return ResponseEntity.notFound().build();
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/{id}") // define metodo como put (atualiza objeto existente)
     public ResponseEntity<Pessoa> update(@RequestBody Pessoa pessoa, @PathVariable Long id) {
         Pessoa request = service.update(pessoa, id);
         return ResponseEntity.ok().body(request);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{id}") // define metodo como delete por id
     public ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
